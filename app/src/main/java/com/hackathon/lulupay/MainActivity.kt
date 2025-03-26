1f29190cc5adcd6da35e8ebe9ac904dfb732a7af
Jeri <okaforstarcool@gmail.com>
2025-03-26 13:33:16 +0100

    Added Biometric Authentication

---
+++ app/src/main/java/com/hackathon/lulupay/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.hackathon.lulupay
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.hackathon.lulupay.R
+import com.sdk.lulupay.activity.RemittanceScreen
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(
+            this@MainActivity,
+            RemittanceScreen::class.java
+        )
+
+        val btn: AppCompatButton = findViewById(R.id.btnPayment);
+        btn.setOnClickListener {
+            startActivity(intent)
+        }
+    }
+}
